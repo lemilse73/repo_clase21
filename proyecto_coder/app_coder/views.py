b8b9585f101feff55e1a067d611ff792b3b7d294
Lorena <lemilse@yahoo.com.ar>
2022-05-29 14:16:30 -0400

    Version clase 21

---
+++ proyecto_coder/app_coder/views.py
@@ -0,0 +1,63 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from app_coder.models import Curso
+from django.template import loader
+from app_coder.forms import Curso_formulario
+
+# Create your views here.
+
+def inicio(request):
+    return render (request , "plantilla.html")
+
+
+def cursos (request):
+    cursos = Curso.objects.all()
+    dicc = {"cursos" : cursos}
+    plantilla = loader.get_template ("cursos.html")
+    documento = plantilla.render (dicc)
+    return HttpResponse(documento)
+
+def alta_curso (request, nombre ):
+    curso = Curso (nombre = nombre, camada = 122)
+    curso.save()
+    texto = f"Se guardo en la BD el Curso: {curso.nombre} Camada: {curso.camada}"
+    return HttpResponse (texto)
+    
+def alumnos(request):
+    return render ( request , "alumnos.html" )
+
+def contacto(request):
+    return render ( request , "contacto.html" )
+
+def profesores(request):
+    return render ( request , "profesores.html" )
+
+def curso_formulario(request):
+
+    if request.method == "POST":
+
+        mi_formulario = Curso_formulario( request.POST )
+        if mi_formulario.is_valid():
+            datos = mi_formulario.cleaned_data
+
+
+        curso = Curso( nombre= datos['nombre'] , camada=datos['camada'])
+        curso.save()
+        return render(request , "formulario.html")
+
+    return render (request, "formulario.html")
+
+def buscar_curso(request):
+    return render( request , "buscar_curso.html")
+
+def buscar(request):
+
+    if request.GET['nombre']:
+        nombre = request.GET['nombre']
+        cursos = Curso.objects.filter(nombre__icontains = nombre)
+        return render( request , "resultado_busqueda.html", {"cursos": cursos})
+    else:
+        return HttpResponse(" Campo vacio")
+    
+
+
